fbe360cef9e96bfb55dd94b2847ab642bb8c47dd
MilindSri <milindsrivastava05@gmail.com>
2021-07-05 08:17:38 +0800

    chg1

--- basics.py
+++ basics.py
@@ -2,3 +2,18 @@ day_hours = 24
 week_days = 400
 week_hours = day_hours * week_days
 print(week_hours)
+
+print("total week hours are : " + str(week_hours))
+
+stringname = '123456'
+
+print("Input the first number : ")
+
+pattern = '*'
+i = 0
+j = 4
+while(i < 4):
+    j = j - i - 1
+    while(j > i):
+        print(j * " ")
+        i = i + 1
